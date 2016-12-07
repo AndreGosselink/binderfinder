19fcd2462e36a488ac54334c11430bb9352f6ec2
AndreG andreg <AndreG andreg@miltenyibiotec.de>
2016-12-07 11:48:18 +0100

    postmerge

--- binderfinder.py
+++ binderfinder.py
@@ -1,13 +1,4 @@
 from binderfinder import Matrix
-
-# help(Matrix)
-m = Matrix('./data/example_data.csv', reference=[100, 100], weights=[0.05, 1],
-        annotate='none', stats=True, sort='none', legend='bg', ceil=True,
-        normalize='channels')
-
-m.show_me_where_the_white_rabbit_goes()
-m.save_last_run()
-from binderfinder import Matrix
 import sys
 import os
 
