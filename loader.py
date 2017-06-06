77ddd205a79e604f175e8e30103f4eb68ba57aa8
AndreG andreg@miltenyibiotec.de <andreg@miltenyibiotec.de>
2017-06-06 18:08:31 +0200

    see changelog ;)

--- loader.py
+++ loader.py
@@ -15,11 +15,12 @@ def load_iris(path='iris.data'):
         irisreader.next()
         for row in irisreader:
             sl, sw, pl, pw = map(float, row[:-1])
+            cl = float(row[-1])
             dset['sepal_length'].append(sl)
             dset['sepal_width'].append(sw)
             dset['petal_length'].append(pl)
             dset['petal_width'].append(pw)
-            dset['class'].append(row[-1])
+            dset['class'].append(cl)
     
     dset['sepal_length'] = np.array(dset['sepal_length'])
     dset['sepal_width'] = np.array(dset['sepal_width'])
