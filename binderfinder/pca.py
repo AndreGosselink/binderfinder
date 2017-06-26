dc51daba2748afcbf134ed1910cd447a7b323ba2
AndreG andreg@miltenyibiotec.de <andreg@miltenyibiotec.de>
2017-06-26 16:57:28 +0200

    v1.41 inb4 merge

--- binderfinder/pca.py
+++ binderfinder/pca.py
@@ -2,9 +2,10 @@
 Implemented according to:
 
 'A tutorial on Principal Components Analysis', Lindsay I Smith, 2002
+
+and with ideas from:
+Teddy Roland (https://github.com/teddyroland/python-biplot/blob/master/biplot.py)
 """
-
-
 
 import numpy as np
 from matplotlib.mlab import PCA as mlabPCA
@@ -20,23 +21,33 @@ class PCA(object):
     """
 Input:
 ------------------------
- filename: path to csv file, containing parsable data
+    filename: path to csv file, containing parsable data
 
- standardize: True if input data are to be standardized. If False, only centering will be carried out.
+    centering: centering data by 'mean', 'median' or 'none' centering at all
+
+    normalize: normalize each parameter independently prior PCA
+
+    reduce_to: number of components the data is reduced to. If -1 all components are used
+
+    annotate: annotate datapoint in scatterplot. where 0 is the first data item in the data file
+
+    portions: plot portion of variance for each component
  """
     
 
     def __init__(self, filename,
                  centering='mean',
-                 normalize=True,
+                 normalize=False,
                  reduce_to=-1,
-                 figsize=(4.5, 7),
+                 figsize=(13, 7),
                  debug=False,
                  portions=True,
+                 annotate=False,
                 ):
 
         offset_func = {  'mean': lambda dat: np.mean(dat, axis=1),
                        'median': lambda dat: np.median(dat, axis=1),
+                         'none': lambda dat: dat,
                       }
 
         self.debug = debug
@@ -44,6 +55,7 @@ Input:
         self._figsize = figsize
         self._offset_func = offset_func[centering]
         self._portions = portions
+        self._annotate = annotate
 
         self.parser = parser = Parser(filename)
         self._filename = filename
@@ -82,8 +94,8 @@ Input:
 
         self.pca_transform = self._transform(self.data)
 
-        # nornmalizing
-        self.pca_transform /= np.max(self.pca_transform)
+        # # nornmalizing
+        # self.pca_transform /= np.max(self.pca_transform)
         
         if debug and self.data.shape[0] == 2:
             print 'data'
@@ -116,7 +128,8 @@ Input:
 
     def _standardize_data(self, data):
         if self._normalize:
-            print 'normalizing data...'
+            if self.debug:
+                print 'normalizing data...'
             data = data.copy() 
             for i, p in enumerate(data):
                 data[i] /= np.max(p)
@@ -129,7 +142,8 @@ Input:
         return res
 
     def _check_consistency(self):
-        print 'checking eigenvector properties...',
+        if self.debug:
+            print 'checking eigenvector properties...',
         m = self._covMat.shape[0]
         vecs = self._eigenvecs
         vals = self._eigenvals
@@ -140,76 +154,84 @@ Input:
                 raise ValueError('Eigenvector {} is odd...'.format(i))
             if not np.isclose(np.linalg.norm(vecs[i]), 1.0):
                 raise ValueError('Eigenvector {} is not normed...'.format(i))
-        print 'all good!'
+        if self.debug:
+            print 'all good!'
     
     def show(self):
-        if self._portions:
-            f, ax = plt.subplots(2, figsize=self._figsize)
-            ax[0].scatter(*self.pca_transform[:2,:])
-            if self._normalize:
-                ax[0].set_xlim(-1.3, 1.3)
-                ax[0].set_ylim(-1.3, 1.3)
-            else:
-                xmin, xmax = map(lambda f: f(self.pca_transform[0,:]), [np.min, np.max])
-                ymin, ymax = map(lambda f: f(self.pca_transform[1,:]), [np.min, np.max])
-                # ax[0].set_xlim(xmin, xmax)
-                # ax[0].set_ylim(ymin, ymax)
 
+        xmin, xmax = map(lambda f: f(self.pca_transform[0,:])*1.3, [np.min, np.max])
+        ymin, ymax = map(lambda f: f(self.pca_transform[1,:])*1.3, [np.min, np.max])
+
+        absmin = np.min([xmin, ymin])
+        absmax = np.max([xmax, ymax])
+        scale_to = np.min([np.abs(absmin), absmax]) * 0.8
+
+        sortidx = self._sortidx
+        pcnum = range(self._fVals.size)
+
+        if self._portions:
+            f, ax = plt.subplots(1, 2, figsize=self._figsize)
+            ax[0].scatter(*self.pca_transform[:2,:])
+            ax[0].set_xlim(absmin, absmax)
+            ax[0].set_ylim(absmin, absmax)
             ax[0].set_xlabel('PC 1')
             ax[0].set_ylabel('PC 2')
             ax[0].set_title(self._filename + ' PCA')
+            arrow_ax = ax[0]
 
-            self._draw_arrows(ax[0])
-
-            sortidx = self._sortidx
-            pcnum = range(self._fVals.size)
             ax[1].scatter(pcnum, self._fVals / np.sum(self._fVals))
             ax[1].set_xticks(pcnum)
             ax[1].set_xticklabels(['PC{}'.format(i+1) for i in pcnum])
             ax[1].set_title('Proportion of Variance')
-            
-            f.tight_layout()
         else:
             f, ax = plt.subplots(1, figsize=self._figsize)
             ax.scatter(*self.pca_transform[:2,:])
-            if self._normalize:
-                ax.set_xlim(-1.3, 1.3)
-                ax.set_ylim(-1.3, 1.3)
-            else:
-                xmin, xmax = map(lambda f: f(self.pca_transform[0,:]), [np.min, np.max])
-                ymin, ymax = map(lambda f: f(self.pca_transform[1,:]), [np.min, np.max])
-                # ax[0].set_xlim(xmin, xmax)
-                # ax[0].set_ylim(ymin, ymax)
-
+            ax.set_xlim(absmin, absmax)
+            ax.set_ylim(absmin, absmax)
             ax.set_xlabel('PC 1')
             ax.set_ylabel('PC 2')
             ax.set_title(self._filename + ' PCA')
+            arrow_ax = ax
 
-            self._draw_arrows(ax)
+        print scale_to
+        self._draw_arrows(arrow_ax, scale_to)
+        f.tight_layout()
 
-            f.tight_layout()
+        if self._annotate:
+            self._annotate_plot(ax[0])
+
         plt.show()
 
-    def _draw_arrows(self, ax):
+    def _draw_arrows(self, ax, scale_to):
         n = self._reduce
         # labels = [self.parser.data_layout[self.parser.PARA_LABEL][idx] for idx in self._sortidx]
         labels = self._labels
         if self.debug:
             print 'Picking Arrows and drawing them'
         bivecs = self._fVecs[:,:2]
+        length = np.max(np.sqrt((self._fVecs[:,0] + self._fVecs[:,1])**2))
         if self.debug:
             for i in xrange(2):
                 val = self._fVals[i]
                 vec = bivecs[:,i]
                 tvec = np.dot(self._covMat, vec) / val
                 is_close = np.all(np.isclose(tvec, vec))
-                print is_close,
-                if not is_close:
-                    print '({}) -> ({})'.format(','.join(map('{:.2f}'.format, tvec)), ','.join(map('{:.2f}'.format, vec)))
+                if self.debug:
+                    print is_close,
+                    if not is_close:
+                        print '({}) -> ({})'.format(','.join(map('{:.2f}'.format, tvec)), ','.join(map('{:.2f}'.format, vec)))
         for i in range(n):
             # get vectors
-            tox = bivecs[i,0]
-            toy = bivecs[i,1]
+            tox = bivecs[i,0] / length * scale_to
+            toy = bivecs[i,1] / length * scale_to
             ax.arrow(0, 0, tox, toy, color='r', alpha=0.5)
             ax.text(tox * 1.15, toy * 1.15, labels[i], color='g', ha='center', va='center')
+
+    def _annotate_plot(self, ax):
+        pc0 = self.pca_transform[0,:]
+        pc1 = self.pca_transform[1,:]
+        offset = np.max([pc0, pc1])*0.03
+        for idx, (x, y) in enumerate(zip(pc0, pc1)):
+            ax.text(x, y+offset, str(idx), color=[0.0, 0.0, 0.0, 0.5], ha='center', va='center', size='xx-small')
+
 
