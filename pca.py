9007902b165642f57cd34e2e53113adbd4a71e31
AndreG andreg@miltenyibiotec.de <andreg@miltenyibiotec.de>
2017-06-27 12:51:52 +0200

    qweqw

--- pca.py
+++ pca.py
@@ -1,6 +1,6 @@
 from binderfinder import PCA
 
-p = PCA('./data/mock_data_pca_rnd.csv', annotate=True, normalize=False, covplot=True)
-# p = PCA('./data/iris_dataset/iris.data', annotate=True, normalize=False)
+# p = PCA('./data/mock_data_pca_rnd.csv', annotate=True, normalize=False, covplot=True)
+p = PCA('./data/iris_dataset/iris.data', annotate=True, normalize=False, covplot=True)
 # p = PCA('./data/mock_data_script.csv')
 p.show()
