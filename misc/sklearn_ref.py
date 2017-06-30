3f52cfe005d0121729b2958f654a3446f58c9aff
AndreG andreg@miltenyibiotec.de <andreg@miltenyibiotec.de>
2017-06-30 15:06:03 +0200

    added kernelpca to ref implementation

--- misc/sklearn_ref.py
+++ misc/sklearn_ref.py
@@ -1,12 +1,13 @@
 import pandas as pd
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, KernelPCA
 from matplotlib import pyplot as plt
 import numpy as np
+import IPython as ip
 
 ## import data
 
 # my_csv = './data/mock_data_script_sk.csv' ## path to your dataset
-my_csv = './data/iris_dataset/iris_sk.data' ## path to your dataset
+my_csv = '../data/iris_dataset/iris_sk.data' ## path to your dataset
 # my_csv = './data/mock_data_pca_sk.csv' ## path to your dataset
 
 dat = pd.read_csv(my_csv, sep=';', usecols=xrange(4))
@@ -23,6 +24,8 @@ pca = PCA(n_components = n)
 
 pca.fit(dat)
 
+kpca = KernelPCA(kernel="rbf", fit_inverse_transform=True, gamma=10)
+kpca.fit(dat)
 
 ## project data into PC space
 
@@ -33,8 +36,8 @@ yvector = pca.components_[1]
 xs = pca.transform(dat)[:,0] # see 'prcomp(my_data)$x' in R
 ys = pca.transform(dat)[:,1]
 
-
-
+kxs = kpca.transform(dat)[:,0] # see 'prcomp(my_data)$x' in R
+kys = kpca.transform(dat)[:,1]
 
 ## visualize projections
     
@@ -48,9 +51,15 @@ for i in range(len(xvector[:n])):
     plt.text(xvector[i]*max(xs)*1.2, yvector[i]*max(ys)*1.2,
              list(dat.columns.values)[i], color='r')
 
-for i in range(len(xs)):
-# circles project documents (ie rows from csv) as points onto PC axes
-    plt.plot(xs[i], ys[i], 'bo')
-    # plt.text(xs[i]*1.2, ys[i]*1.2, list(dat.index)[i], color='b')
+# for i in range(len(xs)):
+# # circles project documents (ie rows from csv) as points onto PC axes
+#     plt.plot(xs[i], ys[i], 'bo')
+#     # plt.text(xs[i]*1.2, ys[i]*1.2, list(dat.index)[i], color='b')
+
+plt.scatter(xs, ys)
+
+f, ax2 = plt.subplots(1)
+ax2.scatter(kxs, kys)
+ax2.set_title('kernel PCA')
 
 plt.show()
