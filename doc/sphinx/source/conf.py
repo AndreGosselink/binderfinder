d145f120ce0c2cee87dfecae18674278ee5e6e23
rikisa <andre.gosselink@stud.fh-swf.de>
2017-11-30 12:01:10 +0100

    this kills the man

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../../'))
 
-autodoc_mock_imports = ['matplotlib', 'numpy', 'scipy', 'matplotlib.pyplot', 'matplotlib.widgets', 'matplotlib.gridspec ',
+autodoc_mock_imports = ['matplotlib', 'numpy', 'scipy', 'matplotlib.pyplot', 'matplotlib.widgets', 'matplotlib.gridspec',
         'csv',]
 
 # -- General configuration ------------------------------------------------
