9bbd7fbd8351502c28c3986411d240e6fc2c19d2
rikisa <andre.gosselink@stud.fh-swf.de>
2017-11-30 11:46:12 +0100

    mockimports in doc

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../../'))
 
-autodoc_mock_imports = ['matplotlib', 'numpy', 'scipy']
+autodoc_mock_imports = ['matplotlib', 'numpy', 'scipy', 'matplotlib.pyplot']
 
 # -- General configuration ------------------------------------------------
 
